b78f2dcdcace57979509792ef2756e801011e0ab
0xFlare <flame.protocol@gmail.com>
2021-02-23 15:56:21 +0100

    Fix blockchain export

--- src/constants.ts
+++ src/constants.ts
@@ -3,7 +3,7 @@ import JSBI from 'jsbi'
 // exports for external consumption
 export type BigintIsh = JSBI | bigint | string
 
-export declare enum Blockchain {
+export enum Blockchain {
   ETHEREUM = 1,
   BINANCE_SMART_CHAIN = 2,
   HARMONY = 3
